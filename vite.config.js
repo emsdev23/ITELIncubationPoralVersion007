9c158fde4986e90bccda23a28b0f0ac8e4e32d0f
emsdev23 <devems2023@gmail.com>
2025-12-11 16:43:05 +0530

    11-12-2025

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,21 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "./src/test/setup.js",
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
+    allowedHosts: [
+      "startups.itelfoundation.in",
+      "www.startups.itelfoundation.in",
+    ],
+    proxy: {
+      "/itelinc": {
+        target: "http://10.3.3.160:8086",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
   },
 });
